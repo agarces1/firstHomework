aa149512c4bfee2376c09e84aa15169a56463de8
agarces1 <adriangarces1@gmail.com>
2021-05-21 23:33:13 -0500

    c++ basic strings

---
+++ firstHomework/Source.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "*******************************************" << endl;
+    cout << "*               Student Info \u2211              *" << endl;
+    cout << "*******************************************" << endl;
+
+    cout << "Name: Adrian Arroyo\nMajor: undecided but want to code \nPrior Coding Experience?: previous clases\nCredit hours this semester: 7\nOther interests: growing plants and video games" << endl;
+
+    cout << "*******************************************" << endl;
+    cout << "*           \u274C   Favorite Things \u274C             *" << endl;
+    cout << "*******************************************" << endl;
+    cout << "Favorite color: Crimson\nFavorite Video Games:Phasmophobia, GTA5, SCP Secret Laboratory,The Stanley Parable\nFavorite Hobby:Playing guitar and daydreaming\nFavorite food:Tacos, tortas and goat stew." << endl;
+
+
+    return 0;
+}
+
+
